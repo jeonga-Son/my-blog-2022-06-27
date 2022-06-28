5cfc84a0b964695880fef134c97e393c747dcdb1
jeonga-Son <jjeong0106@gmail.com>
2022-06-28 23:11:04 +0900

    37강까지 완료_수정2

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,15 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: `https://www.yourdomain.tld`,
     title: "손정아의 개발 블로그",
-    description: "이 사이트에 개발자 손정아의 개발일지가 담겨있습니다.",
   },
-  plugins: ["gatsby-plugin-image", "gatsby-plugin-sharp", {
-    resolve: "gatsby-source-filesystem",
-    options: {
-      name: `blog`,
-      path: `${__dirname}/blog`,
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
       },
     },
     "gatsby-plugin-mdx",
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,14 +1,13 @@
-import React from "react"
+import React from "react";
 import Layout from "../components/layout";
 
 function AboutPage() {
-    return (
-        <main>
-            <Layout pageTitle="About Me">
-            <h1>자기소개 페이지 입니다.</h1>
-            </Layout>
-        </main>
-    );
-};
-
+  return (
+    <Layout pageTitle="About Me">
+      <p>
+        Hi there! I'm the proud creator of this site, which I built with Gatsby.
+      </p>
+    </Layout>
+  );
+}
 export default AboutPage;
