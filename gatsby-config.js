c4f1b83e320fd0e2debababd9645213684d9a0f3
jeonga-Son <jjeong0106@gmail.com>
2022-06-28 21:49:35 +0900

    220628_2149

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     siteUrl: `https://www.yourdomain.tld`,
-    title: "My First Gatsby Site",
+    title: "손정아의 개발 블로그",
     description: "이 사이트에 개발자 손정아의 개발일지가 담겨있습니다.",
   },
   plugins: ["gatsby-plugin-image", "gatsby-plugin-sharp", {
--- src/pages/blog/(mdx_slug).js
+++ src/pages/blog/(mdx_slug).js
@@ -1,26 +1,10 @@
 import React from "react";
-import { graphql } from "gatsby";
-import { MDXRenderer } from "gatsby-plugin-mdx";
 import Layout from "../../components/layout";
-
-const BlogPost = ({ data }) => {
+const BlogPost = () => {
   return (
-
-    <Layout pageTitle={data.mdx.frontmatter.title}>
-      <p>{data.mdx.frontmatter.date}</p>
-      <MDXRenderer>{data.mdx.body}</MDXRenderer>
+    <Layout pageTitle="Super Cool Blog Posts">
+      <p>My blog post contents will go here (eventually).</p>
     </Layout>
   );
 };
-export const query = graphql`
-  query ($id: String) {
-    mdx(id: { eq: $id }) {
-      frontmatter {
-        title
-        date(formatString: "MMMM D, YYYY")
-      }
-      body
-    }
-  }
-`;
 export default BlogPost;
