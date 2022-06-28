6fb193f49209cb73e83ecbdcecd92f7654923c23
jeonga-Son <jjeong0106@gmail.com>
2022-06-28 17:16:31 +0900

    블로그 포스트 리스트

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,14 +1,28 @@
-import React from "react"
+import { graphql, useStaticQuery } from "gatsby";
+import React from "react";
 import Layout from "../components/layout";
 
 function BlogPage() {
-    return (
-        <main>
-            <Layout pageTitle="My Blog Posts">
-             <p>My cool posts will go in here</p>
-            </Layout>
-        </main>
-    );
-};
+  const data = useStaticQuery(graphql`
+    query {
+      allFile {
+        nodes {
+          name
+        }
+      }
+    }
+  `);
 
+  return (
+    <Layout pageTitle="My Blog Posts">
+      <p>My cool posts will go in here</p>
+
+      <ul>
+        {data.allFile.nodes.map((node) => (
+          <li key={node.name}>{node.name}</li>
+        ))}
+      </ul>
+    </Layout>
+  );
+}
 export default BlogPage;
